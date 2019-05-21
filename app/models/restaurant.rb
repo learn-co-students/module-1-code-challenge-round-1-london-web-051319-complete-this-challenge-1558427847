c1dae6395034371f03d3796abffe76e7b16d6b41
Alex Johnston <alexjohnston@ymail.com>
2019-05-21 10:06:50 +0100

    Done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,35 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  def customers
+    reviews.map(&:customer).uniq
+  end
+
+  def average_star_rating
+    reviews.map(&:rating).sum/reviews.size.to_f
+  end
+
+  def longest_review
+    reviews.max_by{|review| review.content.length}
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|restaurant| restaurant.name == name}
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,19 @@
 class Review
-  
-end
 
+  attr_accessor :restaurant, :customer, :content, :rating
+
+  @@all = []
+
+  def initialize(restaurant, customer, content, rating)
+    @restaurant = restaurant
+    @customer = customer
+    @content = content
+    @rating = rating
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
