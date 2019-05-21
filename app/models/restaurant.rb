d19977e987610a1525c7913083fed9d9866c8177
chrisdews <cldews1@gmail.com>
2019-05-21 10:42:57 +0100

    chris dews - ran out of time

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,46 @@
 class Restaurant
-  attr_reader :name
+  attr_reader :rest_name
 
-  def initialize(name)
-    @name = name
+  @@all = []
+
+  def initialize(rest_name:)
+    @rest_name = rest_name
   end
+
+  def self.all
+  	@@all
+  end
+
+  def reviews
+  	Review.all.select {|review| review.restaurant == self}
+
+  end
+
+  def customers
+  	reviews.map { |review| review.customer }.uniq
+  end
+
+  def average_star_rating
+  	count = 0
+  	reviews.select {|review| count += review.rating}
+  	count.to_f / reviews.count.to_f
+  end
+
+  def longest_review
+  	count = 0
+  	reviews.each do |review|
+  		if review.content.length > count
+  			count = review.content.length
+  		end
+  	end
+  	count
+
+  	# this does a count not returning the review!
+  end
+
+  def self.find_by_name(name)
+  	self.all.select {|restaurant| restaurant.rest_name  == name}
+  end
+  	# doesnt work?
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,24 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+rest1 = Restaurant.new(rest_name: "Bobby's cakes")
+rest2 = Restaurant.new(rest_name: "Jimmy's chips")
+rest3 = Restaurant.new(rest_name: "Sarah's burgers")
+
+cust1 = Customer.new(first_name: "Chris", last_name: "Dews")
+cust2 = Customer.new(first_name: "James", last_name: "Day")
+
+
+review1 = Review.new(restaurant: rest1, rating: 4, content: "i liked it", customer: cust1)
+review2 = Review.new(restaurant: rest2, rating: 3, content: "ok", customer: cust1)
+review3 = Review.new(restaurant: rest1, rating: 4, content: "i <3 it", customer: cust2)
+review4 = Review.new(restaurant: rest3, rating: 5, content: "i liked it very much", customer: cust2)
+review5 = Review.new(restaurant: rest3, rating: 4, content: "i liked it ok", customer: cust1)
+
+
+
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
