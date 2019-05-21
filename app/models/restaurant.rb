d089e75b90eb58c5e7f87fe213726efaf4537561
mbarrins <maija.barrins@gmail.com>
2019-05-21 10:04:27 +0100

    completed required methods and properties

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,34 @@
 class Restaurant
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def reviews
+  	Review.all.select{|review| review.restaurant == self}
+  end
+
+  def customers
+  	reviews.map{|review| review.customer}.uniq
+  end
+
+  def average_star_rating
+  	reviews.reduce(0){|sum, review| sum + review.rating}/reviews.length.to_f
+  end
+
+  def longest_review
+  	reviews.max_by{|review| review.content.length}
+  end
+
+  def self.all
+  	@@all
+  end
+
+  def self.find_by_name(name)
+  	self.all.find{|restaurant| restaurant.name == name}
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,19 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+customer1 = Customer.new("First1", "Last1")
+customer2 = Customer.new("First", "Last2")
+customer3 = Customer.new("First", "Last3")
+
+restaurant1 = Restaurant.new("Restaurant1")
+restaurant2 = Restaurant.new("Restaurant2")
+
+review1 = Review.new(customer1, restaurant1, "Good", 3)
+review2 = Review.new(customer2, restaurant2, "Poor", 2)
+review3 = Review.new(customer3, restaurant1, "Excellent", 5)
+review4 = Review.new(customer1, restaurant2, "OK", 4)
+review5 = Review.new(customer2, restaurant1, "Good", 3)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
