b9bc0b1140c1cfda3545d8c95621d5dba252ff69
tstiddy <spaceinvadini99@gmail.com>
2019-05-21 10:41:46 +0100

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,18 @@
 class Review
-  
-end
 
+  attr_accessor :customer, :restaurant, :review
+
+  @@all = []
+
+  def initialize(customer, restaurant, review)
+    @customer = customer
+    @restaurant = restaurant
+    @review = review
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
