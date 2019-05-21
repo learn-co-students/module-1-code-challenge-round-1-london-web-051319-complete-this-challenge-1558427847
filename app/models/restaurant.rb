dcae3557ddbfb0c538cdd0095cd01d66f82beedc
github username <github email address>
2019-05-21 10:40:58 +0100

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,30 @@
 class Restaurant
-  attr_reader :name
+  include Helper
+  extend Finder
+  def initialize(*args)
+    vars = %w(name)
+    super(vars, args)
+  end
 
-  def initialize(name)
-    @name = name
+  def reviews
+    super(&:restaurant)
+  end
+
+  def customers
+    reviews.map(&:customer).uniq
+  end
+
+  def average_star_rating
+    reviews.map(&:rating).sum / reviews.length
+  end
+
+  def longest_review
+    reviews.map(&:content).max_by(&:length)
+  end
+
+
+  def self.find_by_name(name)
+    finder(name, &:name)
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,16 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+first_name = %w(Ewart Fabian Felix Ferdinand Florian Gideon Geoffrey Gregory Hector Horatio Hubert Hugh Hugo Humphrey Ignatius)
+
+last_name = %w(Dexter  Draker Draper Drover Faber Falconer Farrier Faulkner Fisher Fletcher Forester Forster)
+
+restaurants = %w(LettuceEat FilledOfDreams BiteMeSandwiches ThaiTanic JustFalafs GochewGrill ChickenItzaRestaurant)
+
+reviews = %w(nice meh amazing awfull ok fine incredible)
+
+Customer.new(first_name.sample, last_name.sample).add_review(Restaurant.new(restaurants.sample), reviews.sample, rand(5) + 1)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
